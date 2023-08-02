6cdfd8ae9f0fcff1a7bd1e731cc03fc10c1627f5
harunm1 <harun.mohammad2109@gmail.com>
2023-08-02 18:47:31 -0400

    adding auth

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,13 +3,15 @@ from rest_framework import generics
 from .serializers import MenuItemSerializer, BookingSerializer
 from .models import MenuItem, Booking
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
@@ -20,4 +22,4 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
